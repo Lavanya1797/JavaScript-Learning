13c530087b9a7989d047d94d27fab13328d07719
lavanya1797 <lavanyasoundar97@gmail.com>
2021-09-27 17:52:49 +0100

    fundamental 2 completed

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -38,7 +38,7 @@ const jonas = {
     }
 };
 console.log(jonas.getSummary());
-*/
+
 
 const MarkMiller = {
     Name : 'MarkMiller',
@@ -61,8 +61,85 @@ const JohnSmith = {
     }
 };
 const high = function() {
-    return `${(MarkMiller.calcBMI() > JohnSmith.calcBMI()) ? MarkMiller.Name  : JohnSmith.Name} HAS got a higer BMI`;
+    return `${(MarkMiller.calcBMI() > JohnSmith.calcBMI()) ? `${MarkMiller.Name} (${MarkMiller.calcBMI()})` : `${JohnSmith.Name} (${JohnSmith.calcBMI()})`}  HAS got a higer BMI`;
 };
 
 console.log(MarkMiller.calcBMI(), JohnSmith.calcBMI());
 console.log(high());
+
+
+// for loop using years
+const years = [1991,1992,1993,1994];
+const ages = [];
+for (let i=0; i<years.length; i++){
+    console.log(`${2021-years[i]}`);
+}
+const jonas = ['jonas' , 2037-1992, 'teacher'];
+const types = [];
+
+for (let i=0; i<jonas.length; i++){          // i=0 bcz array calculated from 0 and length is 3
+
+console.log ( jonas[i]);
+
+types.push(typeof jonas[i]);              // it add types element to the end of array
+
+}
+//for loop using years
+const years = [1991,1992,1993,1994];
+const ages = [];
+for (let i=0; i<years.length; i++){
+    //console.log(`${2021-years[i]}`);
+    ages.push(2021-years[i]);
+}
+console.log(ages);
+const jonas = ['jonas' , 2037-1992, 'teacher'];
+const types = [];
+
+for (let i=0; i<jonas.length; i++){          // i=0 bcz array calculated from 0 and length is 3
+
+console.log ( jonas[i]);
+
+types.push(typeof jonas[i]);              // it add types element to the end of array
+
+}
+console.log(types);
+
+for (let rep = 1 ; rep<=10; rep++){
+
+    console.log(`Lifting weighs repetition ${rep}`);
+    
+}
+// while loop for dice
+let dice = (Math.trunc(Math.random()*6)) + 1;  // should declare dice first
+while (dice !== 6){
+    console.log(`you rolled ${dice}`);
+    dice = (Math.trunc(Math.random()*6)) + 1; // should write this again here otherwise loop will not be ended
+    if (dice==6) console.log(`you rolled ${dice}`);
+}*/
+
+// tip calculator using loops
+const bill = [22,295,176,440,37,105,10,1100,86,52];
+const tips = [];
+const totals = [];
+const calcTip = function(bill){
+    return bill>=50 && bill<=300 ? bill*0.15 : bill*0.02;
+};
+for (let i=0; i<bill.length; i++){
+    let tip = (calcTip(bill[i]));
+    tips.push(tip);
+    totals.push(tip+bill[i]);
+}
+console.log(bill,tips);
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      // sum = sum + arr[i];
+      sum += arr[i];
+    }
+    return sum / arr.length;
+  }
+  // eg:  console.log(calcAverage([2, 3, 7]));
+  console.log(calcAverage(totals));
+  console.log(calcAverage(tips));
+
+
